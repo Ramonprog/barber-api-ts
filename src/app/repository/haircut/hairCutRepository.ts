686d2a60f91065bce9c93d2a96ccd502cde0dedb
Ramonprog <programador.ramon@gmail.com>
2024-06-09 10:06:21 -0300

    feat: limitando quantidade de cortes de cabelo para não assinantes

--- src/app/repository/haircut/hairCutRepository.ts
+++ src/app/repository/haircut/hairCutRepository.ts
@@ -7,8 +7,8 @@ export class HairCutRepository {
     this.prisma = new PrismaClient();
   }
 
- async createHairCut({name,price}: CreateHairCutDto, user_id:string): Promise<CreateHairCutDto> {
-  if(!user_id || user_id as string === '') throw new Error('User not found')
+  async createHairCut({ name, price }: CreateHairCutDto, user_id: string): Promise<CreateHairCutDto> {
+    if (!user_id || user_id as string === '') throw new Error('User not found')
     try {
       const newHairCut = await this.prisma.haircut.create({
         data: {
@@ -21,6 +21,19 @@ export class HairCutRepository {
     } catch (error) {
       throw new Error(error.message);
     }
-}
-}
+  }
 
+  async countHairCut(id: string) {
+    try {
+      const haircuts = await this.prisma.haircut.count({
+        where: {
+          user_id: id
+        }
+      });
+      return haircuts;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+}
+  
--- src/app/services/haircut/hairCutService.ts
+++ src/app/services/haircut/hairCutService.ts
@@ -1,16 +1,28 @@
+import { HttpException } from "../../../types/HttpException";
 import { CreateHairCutDto, createHairCutSchema } from "../../dto/createHairCut";
 import { HairCutRepository } from "../../repository/haircut/hairCutRepository";
+import { UserRepository } from "../../repository/user/userRepository";
 
 export class HairCutService {
   private hairCutRepository: HairCutRepository;
+  private userRepository: UserRepository;
 
   constructor(hairCutRepository: HairCutRepository) {
     this.hairCutRepository = hairCutRepository;
+    this.userRepository = new UserRepository();
   }
 
   async createHairCut(CreateHairCutDto:CreateHairCutDto, user_id: string): Promise<CreateHairCutDto> {
  
     try {
+      const hairCutQuantity = await this.hairCutRepository.countHairCut(user_id);
+    
+      const user = await this.userRepository.findUserById(user_id);
+   
+      if(hairCutQuantity >= 2 && user?.subscriptions?.status !== 'active') {
+        throw new HttpException(401, 'You have reached the limit of haircuts, please subscribe to continue using our services');
+      }
+
       const newHairCut = await this.hairCutRepository.createHairCut(CreateHairCutDto, user_id);
       return newHairCut;
     } catch (error) {
